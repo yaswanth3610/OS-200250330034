d1db04533062c212ca58330e5ce565af1d853608
yaswanth3610 <yaswanthkrishna.g@gmail.com>
2020-10-15 22:50:52 +0530

    static and dynamic library

---
+++ arith/sub.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int sub(int a,int b)
+{
+	return a-b;
+}
+
---
+++ staticlib/guntur/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "arith.h"
+
+int main()
+{
+	printf("Friend Add: %d\n",add(50,40));
+	return 0;
+}
